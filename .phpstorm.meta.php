050b40e4f27231b7d44ce42d1d85288b36a6c6c6
gluck <gluck59@gmail.com>
2025-12-06 17:12:04 +0500

    []

---
+++ .phpstorm.meta.php
@@ -0,0 +1,23 @@
+<?php
+namespace PHPSTORM_META {
+    override(
+        \PHPUnit\Framework\TestCase::createMock(0),
+        type(0)
+    );
+    override(
+        \PHPUnit\Framework\TestCase::createStub(0),
+        type(0)
+    );
+    override(
+        \PHPUnit\Framework\TestCase::createConfiguredMock(0),
+        type(0)
+    );
+    override(
+        \PHPUnit\Framework\TestCase::createPartialMock(0),
+        type(0)
+    );
+    override(
+        \PHPUnit\Framework\TestCase::createTestProxy(0),
+        type(0)
+    );
+}
